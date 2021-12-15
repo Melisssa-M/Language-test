86cf8dcf2a13e0603da2a756ec7fd02919ed34db
Melisssa-M <81377061+Melisssa-M@users.noreply.github.com>
2021-12-15 15:33:09 +0200

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,6 +3,6 @@ import time
 from selenium.webdriver.common.by import By
 
 
-def test_language(browser):
+def test_guest_should_see_button(browser):
     button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
     assert browser.find_element(By.CLASS_NAME, 'btn-add-to-basket'), 'There is no button'
